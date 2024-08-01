7c729bda4791656f13d233ca297a34617f35bd55
Ed Page <eopage@gmail.com>
2024-08-01 10:43:19 -0500

    chore: Switch cargo-test-support to registry

--- tests/testsuite/version/workspace_version_exclude/mod.rs
+++ tests/testsuite/version/workspace_version_exclude/mod.rs
@@ -1,14 +1,15 @@
 use cargo_test_support::compare::assert_ui;
+use cargo_test_support::current_dir;
+use cargo_test_support::file;
 
 use crate::git_from;
 use crate::init_registry;
 use crate::CargoCommand;
-use cargo_test_support::curr_dir;
 
 #[cargo_test]
 fn case() {
     init_registry();
-    let project = git_from(curr_dir!().join("in"));
+    let project = git_from(current_dir!().join("in"));
     let project_root = project.root();
     let cwd = &project_root;
 
@@ -26,8 +27,8 @@ fn case() {
         .current_dir(cwd)
         .assert()
         .success()
-        .stdout_matches_path(curr_dir!().join("stdout.log"))
-        .stderr_matches_path(curr_dir!().join("stderr.log"));
+        .stdout_eq(file!["stdout.term.svg"])
+        .stderr_eq(file!["stderr.term.svg"]);
 
-    assert_ui().subset_matches(curr_dir!().join("out"), &project_root);
+    assert_ui().subset_matches(current_dir!().join("out"), &project_root);
 }
